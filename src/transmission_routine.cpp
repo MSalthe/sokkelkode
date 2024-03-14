0321ff4682f276164e3089c43cba1a37f7c10afe
Martin Salthe <martin.salthe@propulsentnu.no>
2024-03-14 15:38:39 +0100

    Shit's working yo

--- src/transmission_routine.cpp
+++ src/transmission_routine.cpp
@@ -1,48 +1,26 @@
 #include "transmission_routine.h"
 #include "config.h"
+#include <ArduinoJson.h>
 
 SensorDataIMU sensor_data;
 
-String package_sensor_data_HTTP(SensorDataIMU* sensor_data, WiFiClient* client) {
-    if (DEBUG) Serial.println("Packaging sensor data for HTTP");
+String package_sensor_data_JSON_lib(SensorDataIMU* sensor_data) {
+    if (DEBUG) Serial.println("Packaging sensor data for JSON lib");
     
-    String package = 
-        "GET /data?IMU=" 
-            + String(sensor_data -> IMU[0]) 
-            + "," + String(sensor_data -> IMU[1]) 
-            + "," + String(sensor_data -> IMU[2]) 
-        + "&gyro=" 
-            + String(sensor_data -> gyro[0]) 
-            + "," + String(sensor_data -> gyro[1]) 
-            + "," + String(sensor_data -> gyro[2])
-        + " HTTP/1.1\r\n" 
-        + "Host: " + SERVER_IP_STRING + "\r\n" 
-        + "Connection: close\r\n\r\n";
+    JsonDocument doc;
 
-    if (DEBUG) Serial.println("Sensor data packaged for HTTP: ");
-    if (DEBUG) Serial.println(package);
+    doc["accel"][0] = sensor_data -> IMU[0];
+    doc["accel"][1] = sensor_data -> IMU[1];
+    doc["accel"][2] = sensor_data -> IMU[2];
 
-    return package;
-}
+    doc["gyro"][0] = sensor_data -> gyro[0];
+    doc["gyro"][1] = sensor_data -> gyro[1];
+    doc["gyro"][2] = sensor_data -> gyro[2];
 
-String package_sensor_data_JSON(SensorDataIMU* sensor_data) {
-    if (DEBUG) Serial.println("Packaging sensor data for JSON");
-    
-    String package = 
-        "{"
-            "\"IMU\": [" 
-                + String(sensor_data -> IMU[0]) 
-                + "," + String(sensor_data -> IMU[1]) 
-                + "," + String(sensor_data -> IMU[2]) 
-            + "],"
-            "\"gyro\": [" 
-                + String(sensor_data -> gyro[0]) 
-                + "," + String(sensor_data -> gyro[1]) 
-                + "," + String(sensor_data -> gyro[2]) 
-            + "]"
-        "}";
+    String package;
+    serializeJson(doc, package);
 
-    if (DEBUG) Serial.println("Sensor data packaged for JSON: ");
+    if (DEBUG) Serial.println("Sensor data packaged for JSON lib: ");
     if (DEBUG) Serial.println(package);
 
     return package;
@@ -50,13 +28,13 @@ String package_sensor_data_JSON(SensorDataIMU* sensor_data) {
 
 void transmit_sensor_data(SensorDataIMU sensor_data, WiFiClient* client) {
     if (DEBUG) Serial.println("Transmitting sensor data");
-    String package = package_sensor_data_JSON(&sensor_data);
+    String package = package_sensor_data_JSON_lib(&sensor_data);
     client -> print(package);
     if (DEBUG) Serial.println("Sensor data transmitted");
-} // Todo error type
+} 
 
 Transmission transmission_routine(WiFiClient* client) {
     sensor_data = simulate_sensor_sample();
     transmit_sensor_data(sensor_data, client);
-    return TRANSMISSION_SUCCESS; // Todo error handling
+    return TRANSMISSION_SUCCESS;
 }
